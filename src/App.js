7e2892755c778f5cbdda10e3a548402d0a108b5f
Lucas-Salomao <lucastadeusalomao@gmail.com>
2024-06-15 11:49:35 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function TypesExample() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Button variant="primary">Primary</Button>{' '}
+      <Button variant="secondary">Secondary</Button>{' '}
+      <Button variant="success">Success</Button>{' '}
+      <Button variant="warning">Warning</Button>{' '}
+      <Button variant="danger">Danger</Button>{' '}
+      <Button variant="info">Info</Button>{' '}
+      <Button variant="light">Light</Button>{' '}
+      <Button variant="dark">Dark</Button>
+      <Button variant="link">Link</Button>
+    </>
   );
 }
+
+const App = () => ( 
+  <Container>
+  <Row>
+  <TypesExample>
+      </TypesExample>
+  </Row>
+</Container>
+);
+
 
 export default App;
